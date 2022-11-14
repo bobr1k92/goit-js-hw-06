86c8eb1c0c2510a3e62cbf0f0c10a09771140391
Denis Mazurov <19bobrik92@gmail.com>
2022-11-14 21:17:47 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorButton = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+const background = document.querySelector('body');
+
+const changeBgColor = () => {
+  colorButton.addEventListener('click', () => {
+     background.style.backgroundColor = getRandomHexColor();
+     colorName.innerHTML = background.style.backgroundColor;
+  });
+};
+changeBgColor();
