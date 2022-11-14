79ebca547ccd40da1a6d0a612d75d50a0e6691b5
Denis Mazurov <19bobrik92@gmail.com>
2022-11-14 20:00:20 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("ul");
+
+const ingridientsList = [];
+
+ingredients.forEach((element) => {
+  const ingredientName = document.createElement("li");
+  ingredientName.textContent = element;
+  ingredientName.classList.add("item");
+  ingridientsList.push(ingredientName);
+
+  return ingredientName;
+});
+
+// console.log(ingridientsList);
+list.append(...ingridientsList);
+console.log(list);
