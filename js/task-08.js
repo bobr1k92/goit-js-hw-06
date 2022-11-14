ea257ee90919fb4ffdb10fd0bdfa2af118ad41ac
Denis Mazurov <19bobrik92@gmail.com>
2022-11-14 20:29:34 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,40 @@
+// const form = document.querySelector('.login-form');
+
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(event) {
+//     event.preventDefault();
+
+//     const formData = new FormData(event.currentTarget);
+
+//     formData.forEach((value, name) => {
+
+//         console.log('onFormSubmut -> name', name);
+//         console.log('onFormSubmut -> value', value);
+//     });
+// }
+
+
 const form = document.querySelector('.login-form');
 
-form.addEventListener('submit', onFormSubmit);
+form.addEventListener('submit', onFormSubmit => {
+    onFormSubmit.preventDefault();
 
-function onFormSubmit(event) {
-    event.preventDefault();
+  const {
+    elements: { email, password },
+  } = onFormSubmit.currentTarget;
 
-    const formData = new FormData(event.currentTarget);
+  const data = new FormData(onFormSubmit.target);
 
-    formData.forEach((value, name) => {
-
-        console.log('onFormSubmut -> name', name);
-        console.log('onFormSubmut -> value', value);
+  if (email.value === '' || password.value === '') {
+    alert('Error! All fields must be filled in');
+  } else {
+    let values = {};
+    data.forEach((value, key) => {
+      values[key] = value;
     });
-}
+    console.log(values);
+    alert('You are successfully logged in ');
+  }
+  onFormSubmit.currentTarget.reset();
+});
